7ed358abb65e261e514eea0a95c905e608e2b690
devanshuguptaa <118649603+devanshuguptaa@users.noreply.github.com>
2025-04-10 19:59:45 +0530

    testing commit

--- pages/8_SMOTE_TOMEK.py
+++ pages/8_SMOTE_TOMEK.py
@@ -15,10 +15,12 @@ Combining both gives a better decision boundary and improves model generalizatio
 """)
 
 # Show diagram image
+
 try:
     st.image("assets\SMOTE.png", caption="Visual Representation: SMOTE + Tomek Technique", use_container_width=True)
 except Exception as e:
     st.error(f"Failed to load image: {e}")
+
 
 
 
